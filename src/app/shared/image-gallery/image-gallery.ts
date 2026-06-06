31e0a42909cc2d7d7c11a68a0ffc41ee6df1daed
harikrishnan-ps <22cs346@mgits.ac.in>
2026-06-06 17:11:24 +0530

    chore: destructively remove application source code from master branch

--- src/app/shared/image-gallery/image-gallery.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-image-gallery',
-  imports: [],
-  templateUrl: './image-gallery.html',
-  styleUrl: './image-gallery.css',
-})
-export class ImageGallery {}
--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: '/api',
-  appName: 'OLX',
-  version: '1.0.0-dev',
-  mockDelay: 200,
-};
